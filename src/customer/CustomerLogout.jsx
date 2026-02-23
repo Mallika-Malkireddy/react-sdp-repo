c561518a3692ff444aef9c8ee66d625c70bcedb2
mallika <2500031370@kluniversity.in>
2026-02-23 14:16:59 +0530

    project v2.0

--- src/customer/CustomerLogout.jsx
+++ src/customer/CustomerLogout.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-export default function CustomerLogOut() {
+export default function CustomerLogout() {
   return (
-    <div>CustomerLogOut</div>
-  )
+    <div>
+      <h2>Customer Logout</h2>
+    </div>
+  );
 }
